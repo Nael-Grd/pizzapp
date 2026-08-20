8d0566aba95fc9cefee452801f22fcd256478ffc
Nael <nael2407@gmail.com>
2026-08-21 00:31:51 +0200

    Add errros

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,81 +8,114 @@ import { getPizzas, getIngredients, postIngredient, deleteIngredient, postPizza,
 
 
 function App() {
-  const [ingredients, setIngredients] = useState([])
-  const [newIngredient, setNewIngredient] = useState("")
+    const [ingredients, setIngredients] = useState([])
+    const [newIngredient, setNewIngredient] = useState("")
 
-  const [pizzas, setPizzas] = useState([])
-  const [nouvelleBase, setNouvelleBase] = useState("Tomate")
-  const [nouveauNom, setNom] = useState("")
-  const [nouveauPrix, setPrix] = useState(0)
-  const [estVege, setEstVege] = useState(false)
-  const [ingredientsSelectionnes, setIngredientsSelectionnes] = useState([])
+    const [pizzas, setPizzas] = useState([])
+    const [nouvelleBase, setNouvelleBase] = useState("Tomate")
+    const [nouveauNom, setNom] = useState("")
+    const [nouveauPrix, setPrix] = useState(0)
+    const [estVege, setEstVege] = useState(false)
+    const [ingredientsSelectionnes, setIngredientsSelectionnes] = useState([])
 
-  const [chargementPizzas, setChargementPizzas] = useState(true)
-  const [chargementIngredients, setChargementIngredients] = useState(true)
+    const [chargementPizzas, setChargementPizzas] = useState(true)
+    const [chargementIngredients, setChargementIngredients] = useState(true)
 
-  const chargerIngredients = () => {
-    getIngredients().then(donnes => {
-      setIngredients(donnes)
-    })
-    .then(() => {
-        setChargementIngredients(false)
-    })
-  }
-  
-  const ajouterIngredient = (e) => {
-    e.preventDefault()   // empeche la page de se recherger/clignoter
-    postIngredient(newIngredient).then(() => {   // fonction felchée anonyme pour attendre la rep du serveur avant execution
-        setNewIngredient("") // On vide le champ texte
-        chargerIngredients() // On recharge la liste
-    })
-  }
+    const [erreurReseau, setErreurReseau] = useState(null)
+    const [erreurAction, setErreurAction] = useState(null)
 
-  const supprimerIngredient = (id) => {
-    deleteIngredient(id).then(() => {
-      chargerIngredients()
-    })
-  }
-
-  const chargerPizzas = () => {
-    getPizzas().then(donnes => {   //return pour que App sache quand la prommesse est finie
-        setPizzas(donnes)
-    })
-    .then(() => {
-        setChargementPizzas(false)
-    })
-  }
-
-  const creerPizza = (e) => {
-    e.preventDefault()
-    postPizza(nouveauNom, nouvelleBase, nouveauPrix, estVege, ingredientsSelectionnes).then(() => {
-        chargerPizzas()
-        setNom("")
-        setPrix(0)
-        setIngredientsSelectionnes([])
-        setEstVege(false)
-    })
-  }
-
-  const supprimerPizza = (id) => {
-    deletePizza(id).then(() => {
-        chargerPizzas()
-    })
-  }
-
-  useEffect(() => {
-    chargerPizzas()
-    chargerIngredients()
-  }, [])
-  
-
-    if(chargementIngredients) {
-        return <div>Livraison des ingredients en cours...</div>
+    const chargerIngredients = () => {
+        getIngredients()
+        .then(donnes => {
+            setIngredients(donnes)
+            setErreurReseau(null)
+        })
+        .catch(erreur => {
+            setErreurReseau('Erreur : serveur injoingnable')
+        })
+        .finally(() => {
+            setChargementIngredients(false)
+        })
+    }
+    
+    const ajouterIngredient = (e) => {
+        e.preventDefault()   // empeche la page de se recherger/clignoter
+        postIngredient(newIngredient)
+        .then(() => {     // fonction felchée anonyme pour attendre la rep du serveur avant execution
+            setNewIngredient("") // On vide le champ texte
+            chargerIngredients() // On recharge la liste
+        })
+        .catch(erreur => {
+            setErreurAction("Impossible d'ajouter l'ingrédient")
+            setTimeout(() => {
+                setErreurAction(null)
+            }, 3000);
+        })
+        
     }
 
-    if(chargementPizzas) {
-        return <div>Cuisson en cours...</div>
+    const supprimerIngredient = (id) => {
+        deleteIngredient(id)
+        .then(() => {
+            chargerIngredients()
+        })
+        .catch(erreur => {
+            setErreurAction("Impossible de supprimer l'ingrédient")
+            setTimeout(() => {
+                setErreurAction(null)
+            }, 3000);
+        })
     }
+
+    const chargerPizzas = () => {
+        getPizzas()
+        .then(donnes => { 
+            setPizzas(donnes)
+            setErreurReseau(null)
+        })
+        .catch(erreur => {
+            setErreurReseau('Erreur : serveur injoingnable')
+        })
+        .finally(() => {
+            setChargementPizzas(false)
+        })
+    }
+
+    const creerPizza = (e) => {
+        e.preventDefault()
+        postPizza(nouveauNom, nouvelleBase, nouveauPrix, estVege, ingredientsSelectionnes)
+        .then(() => {
+            chargerPizzas()
+            setNom("")
+            setPrix(0)
+            setIngredientsSelectionnes([])
+            setEstVege(false)
+        })
+        .catch(erreur => {
+            setErreurAction("Impossible d'ajouter la pizza")
+            setTimeout(() => {
+                setErreurAction(null)
+            }, 3000);
+        })
+    }
+
+    const supprimerPizza = (id) => {
+        deletePizza(id)
+        .then(() => {
+            chargerPizzas()
+        })
+        .catch(erreur => {
+            setErreurAction("Impossible de supprimer la pizza")
+            setTimeout(() => {
+                setErreurAction(null)
+            }, 3000);
+        })
+    }
+
+    useEffect(() => {
+        chargerPizzas()
+        chargerIngredients()
+    }, [])
 
     return (
         <BrowserRouter>
@@ -92,6 +125,19 @@ function App() {
                 <Link className='hover:text-yellow-500' to={"/admin"}>Administration</Link>
                 <Link className='hover:text-yellow-500' to={"/"}>Retour au menu</Link>
             </nav>
+
+            {erreurReseau && 
+                <div className="bg-red-500 text-white font-bold p-4 text-center">
+                    {erreurReseau}
+                </div>
+            }
+
+            {erreurAction && (
+                <div className="fixed bottom-4 right-4 bg-red-600 text-white px-6 py-3 rounded-md shadow-lg font-bold animate-bounce">
+                    {erreurAction}
+                </div>
+)}
+
             <Routes>
                 <Route path='/' element={<Accueil pizzas={pizzas} supprimerPizza={supprimerPizza} chargementPizzas={chargementPizzas} />} />
                 <Route path='/admin' element={<Admin ajouterIngredient={ajouterIngredient} newIngredient={newIngredient} setNewIngredient={setNewIngredient} 
